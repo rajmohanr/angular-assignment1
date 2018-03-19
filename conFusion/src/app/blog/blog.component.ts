e173515dc5b9b2df36c2b9f853a250a0814901fb
Mohan Raj <cutemohan007@gmail.com>
2018-03-19 13:49:58 +0530

    Update blog.component.ts

--- conFusion/src/app/blog/blog.component.ts
+++ conFusion/src/app/blog/blog.component.ts
@@ -12,12 +12,13 @@ export class BlogComponent implements OnInit {
 
    // blogs: Blog[];
   // private blogs  = [];
+  data: any;
   private blogs: Blog[] = [];
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
    const responseData =  this.http.get('./assets/data/blogData.json')
-        .subscribe(response => console.log(response));
+        .subscribe(response => this.data);
         // console.log('data' + responseData);
         // blogs: responseData;
   }
